5463cad7aaf90d4a0aa39806afb74a83a31dfd34
Luis Henrique <luishenriqueribeiro3@gmail.com>
2023-08-24 23:46:14 -0300

    praticando o metodo for utilizando listas

--- array/procurandoNaLista.js
+++ array/procurandoNaLista.js
@@ -6,10 +6,12 @@ const listDeAlunosEMedias = [alunos , notas];
 function exibeNomeEnota(aluno) {
     if(listDeAlunosEMedias[0].includes(aluno)) {
         console.log(`O aluno: ${aluno}, está na lista de alunos e sua nota é:`);
+        const alunos = listDeAlunosEMedias [0];
+        const notas = listDeAlunosEMedias [1];
 
-        const indice = listDeAlunosEMedias[0].indexOf(aluno);
+        const indice =alunos.indexOf(aluno);
 
-        const mediaDoAluno = listDeAlunosEMedias[1][indice];
+        const mediaDoAluno = notas [indice];
 
         console.log(mediaDoAluno);
     }else{
@@ -17,4 +19,4 @@ function exibeNomeEnota(aluno) {
     }
 }
 
-exibeNomeEnota("joao");
+exibeNomeEnota("juliana");
